8bd8d80032c152d57ce9947536e8a5a952bf0180
taoliang1985@gmail.com <taoliang1985@gmail.com@191c2ca7-3885-2a62-3bbc-e3df3e48f935>
2014-06-05 12:41:35 +0000

    加入zxing包 初步蓝牙功能

--- source/LeavePhoneAlone/src/com/ranger/lpa/connectity/bluetooth/LPABlueToothManager.java
+++ source/LeavePhoneAlone/src/com/ranger/lpa/connectity/bluetooth/LPABlueToothManager.java
@@ -1,16 +1,49 @@
 package com.ranger.lpa.connectity.bluetooth;
 
-import android.content.Context;
+import java.io.IOException;
+import java.util.UUID;
 
+import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * 
+* @Description: TODO
+* 
+* @author taoliang(taoliang@baidu-mgame.com)
+* @date 2014年6月3日 下午7:19:36 
+* @version V
+*
+ */
 public class LPABlueToothManager {
 
-	private LPABlueToothManager _instance;
+	private static LPABlueToothManager _instance;
+	private BluetoothManager mBluetoothManager;
+	private BluetoothAdapter mBluetoothAdapter;
+	private BluetoothDevice mBluetoothDevice;
+	private Context mContext;
 	
 	private LPABlueToothManager(Context context) {
-	
+		
+		mContext = context;
+		
+		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		
+		if(mBluetoothAdapter == null){
+			
+		}
+		
+		mBluetoothManager = (BluetoothManager) mContext.getSystemService(Service.BLUETOOTH_SERVICE);
+
 	};
 	
-	public LPABlueToothManager getInstance(Context context) {
+	public static synchronized LPABlueToothManager getInstance(Context context) {
 		
 		if(_instance == null){
 			_instance = new LPABlueToothManager(context);
@@ -18,4 +51,18 @@ public class LPABlueToothManager {
 		
 		return _instance;
 	}
+
+	public BluetoothManager getmBluetoothManager() {
+		return mBluetoothManager;
+	}
+
+	public BluetoothAdapter getmBluetoothAdapter() {
+		return mBluetoothAdapter;
+	}
+
+	public BluetoothDevice getmBluetoothDevice() {
+		return mBluetoothDevice;
+	}
+	
+	
 }
