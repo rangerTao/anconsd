d33a03aba089874152f64b44749ff34cd415c4b7
taoliang1985@gmail.com <taoliang1985@gmail.com@191c2ca7-3885-2a62-3bbc-e3df3e48f935>
2014-06-15 09:56:54 +0000

    sync

---
+++ source/LeavePhoneAlone/src/com/ranger/lpa/ui/view/LPAKeyGuardView.java
@@ -0,0 +1,20 @@
+package com.ranger.lpa.ui.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by taoliang on 14-6-13.
+ */
+public class LPAKeyGuardView extends View {
+
+
+    public LPAKeyGuardView(Context context) {
+        super(context);
+    }
+
+    public LPAKeyGuardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
