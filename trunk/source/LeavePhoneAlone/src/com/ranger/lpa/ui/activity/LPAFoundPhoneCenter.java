8f2a2bb288ca4242b73f66da317f943262dc638a
taoliang1985@gmail.com <taoliang1985@gmail.com@191c2ca7-3885-2a62-3bbc-e3df3e48f935>
2014-07-08 14:10:59 +0000

    一些界面的更新

--- trunk/source/LeavePhoneAlone/src/com/ranger/lpa/ui/activity/LPAFoundPhoneCenter.java
+++ trunk/source/LeavePhoneAlone/src/com/ranger/lpa/ui/activity/LPAFoundPhoneCenter.java
@@ -1,6 +1,8 @@
 package com.ranger.lpa.ui.activity;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
@@ -16,12 +18,16 @@ import android.view.View;
 import android.view.ViewStub;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.ranger.lpa.Constants;
 import com.ranger.lpa.R;
 import com.ranger.lpa.connectity.bluetooth.LPABlueToothManager;
 import com.ranger.lpa.pojos.IncomeResult;
+import com.ranger.lpa.ui.view.LPAKeyGuardView;
+
+import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -29,6 +35,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.zip.Inflater;
 
 /**
  * Created by taoliang on 14-7-5.
@@ -39,6 +46,7 @@ public class LPAFoundPhoneCenter extends BaseActivity implements View.OnClickLis
     View view_phone_found;
 
     FrameLayout fl_btn_start_lock;
+    private FrameLayout fl_start_to_lock;
     TextView tv_cancel_finding;
 
     String blueName = "LPA";
@@ -68,7 +76,6 @@ public class LPAFoundPhoneCenter extends BaseActivity implements View.OnClickLis
 
     private void initView() {
         view_find_phone = ((ViewStub) findViewById(R.id.stub_lock_center)).inflate();
-//
 
         fl_btn_start_lock = (FrameLayout) view_find_phone.findViewById(R.id.fl_search_btn);
         fl_btn_start_lock.setOnClickListener(this);
@@ -97,6 +104,13 @@ public class LPAFoundPhoneCenter extends BaseActivity implements View.OnClickLis
 
                 startBlueToothAndDiscovery();
 
+                setFindingPhoneView();
+
+                View lock_view = View.inflate(this,R.layout.layout_locked_view,null);
+                LPAKeyGuardView lpa = LPAKeyGuardView.getInstance(this);
+                lpa.setLockView(lock_view);
+                lpa.lock();
+
                 break;
             case R.id.btn_cancel_finding:
 
@@ -109,8 +123,15 @@ public class LPAFoundPhoneCenter extends BaseActivity implements View.OnClickLis
                     }
                 }
                 break;
+            case R.id.tv_phone_name_found_small:
+
+                break;
         }
     }
+
+
+    // 查找蓝牙设备
+    private static final int REQUEST_DISCOVERABLE = 0x2;
 
     //开始查找蓝牙设备
     private void startBlueToothAndDiscovery() {
@@ -122,6 +143,11 @@ public class LPAFoundPhoneCenter extends BaseActivity implements View.OnClickLis
 
         btManager.getBluetoothAdapter().setName("test");
         btManager.setDeviceVisiable(this);
+
+        // 设置蓝牙可见
+        Intent enabler = new Intent(
+                BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        startActivityForResult(enabler, REQUEST_DISCOVERABLE);
         new Thread() {
             public void run() {
 
@@ -173,6 +199,8 @@ public class LPAFoundPhoneCenter extends BaseActivity implements View.OnClickLis
                 }
             }
         }.start();
+
+        btManager.startDiscovery();
     }
 
     private void registerFinishReceiver() {
@@ -181,12 +209,57 @@ public class LPAFoundPhoneCenter extends BaseActivity implements View.OnClickLis
         LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(mDiscoveryFinishReceiver, intentDiscoveryFinish);
     }
 
+    private TextView tv_phone_name_founded;
+    private TextView tv_phone_name_small;
+    private BluetoothDevice btDeviceFounded;
+
+    private void setFindingPhoneView(){
+
+        if(tv_phone_name_founded == null) {
+            tv_phone_name_founded = (TextView) view_phone_found.findViewById(R.id.tv_phone_name_found);
+        }
+
+        tv_phone_name_founded.setText(R.string.text_finding_phone_hint);
+
+    }
+
+    private FrameLayout fl_finding_phone_progress;
+
+    private void setFoundedPhoneView(){
+
+        if(fl_start_to_lock == null){
+            fl_btn_start_lock = (FrameLayout) view_phone_found.findViewById(R.id.fl_search_btn);
+        }
+
+        if(fl_finding_phone_progress == null){
+            fl_finding_phone_progress = (FrameLayout) view_phone_found.findViewById(R.id.fl_bind_finding_progress);
+        }
+
+        fl_finding_phone_progress.setVisibility(View.GONE);
+        fl_btn_start_lock.setVisibility(View.VISIBLE);
+
+        btDeviceFounded = btManager.getBluetoothDevices().get(0);
+
+        tv_phone_name_founded = (TextView) view_phone_found.findViewById(R.id.tv_phone_name_found);
+        tv_phone_name_small = (TextView) view_phone_found.findViewById(R.id.tv_phone_name_found_small);
+
+        tv_phone_name_small.setVisibility(View.VISIBLE);
+        tv_phone_name_small.setOnClickListener(this);
+
+        tv_phone_name_small.setText(btDeviceFounded.getName());
+        tv_phone_name_founded.setText(getString(R.string.text_phone_found,btDeviceFounded.getName()));
+
+    }
+
     class DiscoveryFinishReceiver extends BroadcastReceiver {
 
         @Override
         public void onReceive(Context context, Intent intent) {
 
             if (intent.getAction().equals(Constants.action_bt_scan_finish)) {
+
+                showErrorLog("finding finish");
+
                 if (!isLoadingCancel.get()) {
                     LocalBroadcastManager.getInstance(getApplicationContext()).unregisterReceiver(mDiscoveryFinishReceiver);
 
@@ -195,7 +268,12 @@ public class LPAFoundPhoneCenter extends BaseActivity implements View.OnClickLis
                         @Override
                         public void run() {
                             //dismiss loading view. and set the founded view.
-
+                            if(btManager.getBluetoothDevices().size() < 1){
+                                Toast.makeText(getApplicationContext(),"not found",Toast.LENGTH_LONG).show();
+                            }else{
+                                //set the found view
+                                setFoundedPhoneView();
+                            }
                         }
                     });
 
