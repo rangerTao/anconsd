ca11ad084a39096329921e2ae9637b9ad324d18a
taoliang1985@gmail.com <taoliang1985@gmail.com@191c2ca7-3885-2a62-3bbc-e3df3e48f935>
2014-10-27 03:33:26 +0000

    完成搜索扫描view

--- source/LeavePhoneAlone/src/com/ranger/lpa/view/SearchScanView.java
+++ source/LeavePhoneAlone/src/com/ranger/lpa/view/SearchScanView.java
@@ -1,12 +1,16 @@
 package com.ranger.lpa.view;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+
+import com.ranger.lpa.R;
 
 /**
  * Created by taoliang on 14-10-11.
@@ -16,6 +20,8 @@ public class SearchScanView extends View {
     private int degree = 0;
     private int r = 0;
 
+    private Bitmap bmp_search;
+
 
     public SearchScanView(Context context) {
         super(context);
@@ -23,33 +29,38 @@ public class SearchScanView extends View {
 
     public SearchScanView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        bmp_search = BitmapFactory.decodeResource(context.getResources(), R.drawable.btn_search);
+//        setBackgroundResource(R.drawable.btn_red_bg_selector);
     }
 
     public SearchScanView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        bmp_search = BitmapFactory.decodeResource(context.getResources(), R.drawable.btn_search);
+//        setBackgroundResource(R.drawable.btn_red_bg_selector);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        r = getWidth() / 4;
+        r = 50;
 
-        double x = r * Math.cos(degree);
-        double y = r * Math.sin(degree);
+        double x = getWidth() / 2 + r * Math.cos(degree * 3.14 / 180) - (bmp_search.getWidth() /2 );
+        double y = getHeight() / 2 + r * Math.sin(degree * 3.14 / 180) - (bmp_search.getHeight() /2 );
 
         Paint newpaint = new Paint();
         newpaint.setColor(Color.BLACK);
 
-        canvas.drawCircle((float)x,(float)y,5,newpaint);
+        canvas.drawBitmap(bmp_search, (float) x, (float) y, newpaint);
 
-        Log.e("TAG","position of search : x: " + x + "    " + y);
+//        canvas.drawCircle((float) x, (float) y, 5, newpaint);
 
-        degree += 10;
+        degree += 1;
 
         degree = degree % 360;
 
-        postInvalidateDelayed(200);
+        postInvalidateDelayed(10);
 
     }
 }
